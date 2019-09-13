31c3cb64e0916be839484a7e4d2b8f45935b54b0
evergosu <avorochalkin@sedmax.ru>
2019-09-13 10:18:20 +0300

    Changed dist path to match point notation.

--- src/bundler/create-bundler.ts
+++ src/bundler/create-bundler.ts
@@ -6,7 +6,7 @@ import removeRootSyntax from './plugins/root-syntax/remove-root-syntax';
 
 export default function createBundler({ publicUrl }: Bundler.ParcelOptions) {
   return (path: string) => {
-    const bundler = new Bundler(path, { publicUrl });
+    const bundler = new Bundler(path, { publicUrl, outDir: './.dist' });
 
     bundler.on('bundled', bundle => {
       forEachBundle(applyRootSyntaxPlugin, bundle);
